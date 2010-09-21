880209ca23cfeafd32f87c9e0a98559c2d9eaa1d
russoedu <russoedu@ea7316f2-25d1-2a56-c030-ad6d2a9b8e9c>
2010-09-21 04:16:39 +0000

    Curupira1 funcionando (encriptação)

---
+++ trunk/Seguganca/src/util/Printer.java
@@ -0,0 +1,30 @@
+package util;
+
+public class Printer {
+	public static void printVector(String name, byte[] A){
+		System.out.printf(name + ": ");
+		for (int i = 0; i < A.length; i++){ 
+            System.out.printf("%2s ", Conversor.byteToHex(A[i]));
+		}
+		System.out.println();
+	}
+	
+	public static void printVector(byte[] A){
+		System.out.printf("|");
+		for (int i = 0; i < A.length; i++){ 
+            System.out.printf("%2s", Conversor.byteToHex(A[i]));
+            if (i < (A.length - 1)){
+            	System.out.printf(" ");
+            }
+		}
+		System.out.println("|");
+	}
+	
+	public static void printMatrix(String name, byte[][] A){
+		System.out.println(name);
+		for (int i = 0; i < A.length; i++){ 
+			printVector(A[i]);
+		}
+		System.out.println();
+	}
+}
