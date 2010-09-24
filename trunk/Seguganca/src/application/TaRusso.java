5579773b9caa8ea8cd4eec07b13b5cf45102c116
russoedu <russoedu@ea7316f2-25d1-2a56-c030-ad6d2a9b8e9c>
2010-09-24 04:52:49 +0000

    Aplicação com todos os tipos de funções já contruída

--- trunk/Seguganca/src/application/TaRusso.java
+++ trunk/Seguganca/src/application/TaRusso.java
@@ -4,13 +4,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import util.Printer;
+import util.Util;
+
 public class TaRusso {
 	private static InputStreamReader inputStreamReader = new InputStreamReader(System.in);
 	private static BufferedReader reader = new BufferedReader(inputStreamReader);
 	private static String lineRead;
+
+	private static boolean debug = false;
+	
 	private static int keyBits;
 	private static int ivLength;
 	private static int aLength;
+	private static byte[] cipherKey;
+	private static String autenticateDocument;
+	
 	
 	public static void main(String[] args) throws IOException {		
 		System.out.println("************************************************************");
@@ -26,6 +35,12 @@ public class TaRusso {
 		
 		//MAC input
 		aLengthInput();
+
+		//Cipher Key input
+		cipherKeyInput();
+		
+		//Autenticate Document
+		autenticateDocumentInput();
 	}
 	private static void keySizeInput(){
 		//Instructions string
@@ -78,7 +93,7 @@ public class TaRusso {
 					ivLength = intValue;
 					
 					//Output
-					System.out.println("IV terá " + intValue + " bits.\n");
+					System.out.println("IV terá " + intValue + " bits.");
 					
 					validValue = true;
 				}
@@ -110,7 +125,7 @@ public class TaRusso {
 					aLength = intValue;
 					
 					//Output
-					System.out.println("IV terá " + intValue + " bits.\n");
+					System.out.println("IV terá " + intValue + " bits.");
 					
 					validValue = true;
 				}
@@ -119,6 +134,93 @@ public class TaRusso {
 				}
 			} catch (Exception e) {
 				System.out.print("Valor inválido. " + instructions);
+			}
+		}
+	}
+	
+	private static void cipherKeyInput(){
+		int maxSize = keyBits / 8;
+		
+		//Instructions string
+		String instructions = "Por favor, digite uma senha de até " + maxSize + " caracteres: ";
+		
+		boolean validValue = false;
+		
+		System.out.print(instructions);
+		while(!validValue){
+			try {
+				lineRead = reader.readLine().trim();
+				String stringValue = lineRead;
+				int stringSize = lineRead.length();
+				
+				//Validation
+				if(0 <= stringSize && stringSize <= maxSize && !stringValue.equals("")){
+					for(; stringSize < maxSize; stringSize++){
+						stringValue = stringValue.concat("0");
+						if(debug){
+							System.out.print(stringValue);
+							if (stringSize < maxSize - 1)
+								System.out.print( " -> ");
+						}
+					}
+					if(debug)
+						System.out.print("\n");
+					
+					//Value set
+					cipherKey = stringValue.getBytes();
+					
+					//Output
+					System.out.println("Senha adicionada com sucesso");
+					if(debug)
+						Printer.printVector(cipherKey);
+					
+					validValue = true;
+				}
+				else{
+					System.out.print("Valor inválido. " + instructions);
+				}
+			} catch (Exception e) {
+				System.out.print("Valor inválido. " + instructions);
+			}
+		}
+	}
+	
+	private static void autenticateDocumentInput(){
+		//Instructions string
+		String instructions = "Por favor, insira a localização do arquivo a ser autenticado: ";
+		
+		String[] readFile = new String[2];
+		boolean validValue = false;
+		
+		System.out.print(instructions);
+		while(!validValue){
+			try {
+				readFile[0] = reader.readLine().trim();
+//				int intValue = new Integer(lineRead);
+				
+				;
+				
+				//Validation
+				if(Util.readFile(readFile)){
+					
+					//Value set
+//					aLength = intValue;
+					
+					//Output
+					System.out.print("Arquivo \"" + readFile[0] + "\" lido com sucesso.");
+					
+					autenticateDocument = readFile[1];
+					
+					if(debug)
+						System.out.println(readFile[1]);
+					
+					validValue = true;
+				}
+				else{
+					System.out.print("O arquivo \"" + readFile[0] + "\" não foi encontrado. " + instructions);
+				}
+			} catch (Exception e) {
+				System.out.print("O arquivo \"" + readFile[0] + "\" não foi encontrado. " + instructions);
 			}
 		}
 	}
--- trunk/Seguganca/src/util/Util.java
+++ trunk/Seguganca/src/util/Util.java
@@ -1,131 +1,195 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * 
  * Conversor class
- *
+ * 
  */
 public class Util {
 	/**
 	 * Convert a plain text String to an array of bytes
-	 * @param plainText
+	 * 
+	 * @param stringValue
 	 * @return array of bytes
 	 */
-	public static byte[] convertStringToVector(String plainText){
-		if(plainText.length() != 24 && plainText.length() != 36 && plainText.length() != 48){
+	public static byte[] convertStringToVector(String stringValue) {
+		if (stringValue.length() != 24 && stringValue.length() != 36
+				&& stringValue.length() != 48) {
 			return null;
-		}
-		else{
-			int size = plainText.length();
+		} else {
+			int size = stringValue.length();
 			byte[] vectorBlock = new byte[size / 2];
-			for(int i = 0; i < size; i+=2){
-				String sByte1 = plainText.substring(i, i + 1);
-				String sByte2 = plainText.substring(i + 1, i + 2);
-				
-				int byte1 = (stringToByte(sByte1) << (byte)0x04);;
+			for (int i = 0; i < size; i += 2) {
+				String sByte1 = stringValue.substring(i, i + 1);
+				String sByte2 = stringValue.substring(i + 1, i + 2);
+
+				int byte1 = (stringToByte(sByte1) << (byte) 0x04);
+				;
 				int byte2 = stringToByte(sByte2);
-				
-				vectorBlock[i/2] = (byte)(byte1 ^ byte2);
+
+				vectorBlock[i / 2] = (byte) (byte1 ^ byte2);
 			}
 			return vectorBlock;
 		}
 	}
-	
-	public static byte stringToByte(String sByte){
-		if(sByte.equals("0"))
-			return (byte)0x00;
-		else if(sByte.equals("1"))
-			return (byte)0x01;
-		else if(sByte.equals("2"))
-			return (byte)0x02;
-		else if(sByte.equals("3"))
-			return (byte)0x03;
-		else if(sByte.equals("4"))
-			return (byte)0x04;
-		else if(sByte.equals("5"))
-			return (byte)0x05;
-		else if(sByte.equals("6"))
-			return (byte)0x06;
-		else if(sByte.equals("7"))
-			return (byte)0x07;
-		else if(sByte.equals("8"))
-			return (byte)0x08;
-		else if(sByte.equals("9"))
-			return (byte)0x09;
-		else if(sByte.equals("a"))
-			return (byte)0x0A;
-		else if(sByte.equals("b"))
-			return (byte)0x0B;
-		else if(sByte.equals("c"))
-			return (byte)0x0C;
-		else if(sByte.equals("d"))
-			return (byte)0x0D;
-		else if(sByte.equals("e"))
-			return (byte)0x0E;
+
+	public static byte stringToByte(String sByte) {
+		if (sByte.equals("0"))
+			return (byte) 0x00;
+		else if (sByte.equals("1"))
+			return (byte) 0x01;
+		else if (sByte.equals("2"))
+			return (byte) 0x02;
+		else if (sByte.equals("3"))
+			return (byte) 0x03;
+		else if (sByte.equals("4"))
+			return (byte) 0x04;
+		else if (sByte.equals("5"))
+			return (byte) 0x05;
+		else if (sByte.equals("6"))
+			return (byte) 0x06;
+		else if (sByte.equals("7"))
+			return (byte) 0x07;
+		else if (sByte.equals("8"))
+			return (byte) 0x08;
+		else if (sByte.equals("9"))
+			return (byte) 0x09;
+		else if (sByte.equals("a"))
+			return (byte) 0x0A;
+		else if (sByte.equals("b"))
+			return (byte) 0x0B;
+		else if (sByte.equals("c"))
+			return (byte) 0x0C;
+		else if (sByte.equals("d"))
+			return (byte) 0x0D;
+		else if (sByte.equals("e"))
+			return (byte) 0x0E;
 		else
-			return (byte)0x0F;
+			return (byte) 0x0F;
 	}
-	
-	public static String byteToHex(byte b){
+
+	public static String byteToHex(byte b) {
 		return Integer.toString((b & 0xFF) + 0x100, 16).substring(1);
 	}
 
-	public static String byteToHex(byte[] b){
+	public static String byteToHex(byte[] b) {
 		String result = "";
 		for (int i = 0; i < b.length; i++)
 			result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
 		return result;
 	}
-	
+
 	/**
-	 * Convert a vector representing a block to a matrix representing a M2t matrix
-	 * @param block Block of byte
-	 * @param matrix Matrix of byte
-	 * @param columnMapping Boolean
+	 * Convert a vector representing a block to a matrix representing a M2t
+	 * matrix
+	 * 
+	 * @param block
+	 *            Block of byte
+	 * @param matrix
+	 *            Matrix of byte
+	 * @param columnMapping
+	 *            Boolean
 	 */
-	public static void blockToMatrix(byte[] block, byte[][] matrix, boolean columnMapping){
+	public static void blockToMatrix(byte[] block, byte[][] matrix,
+			boolean columnMapping) {
 		int size = block.length / 3;
-		if(columnMapping){
-			for(int i = 0; i < 3; i++) {
-				for(int j = 0; j < size; j++) {
-					matrix[i][j] = block [i + 3 * j];
+		if (columnMapping) {
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < size; j++) {
+					matrix[i][j] = block[i + 3 * j];
 				}
 			}
-		}
-		else{
-			for(int i = 0; i < 3; i++) {
-				for(int j = 0; j < size; j++) {
-					matrix[i][j] = block [size * i + j];
+		} else {
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < size; j++) {
+					matrix[i][j] = block[size * i + j];
 				}
 			}
 		}
 	}
+
 	/**
-	 * Convert a matrix representing a M2t matrix a to vector representing a block
-	 * @param block Block of byte
-	 * @param matrix Matrix of byte
+	 * Convert a matrix representing a M2t matrix a to vector representing a
+	 * block
+	 * 
+	 * @param block
+	 *            Block of byte
+	 * @param matrix
+	 *            Matrix of byte
 	 */
-	public static void matrixToBlock(byte[] block, byte[][] matrix, boolean columnMapping){
+	public static void matrixToBlock(byte[] block, byte[][] matrix,
+			boolean columnMapping) {
 		int size = matrix[0].length;
-		if(columnMapping){
-			for(int i = 0; i < 3; i++) {
-				for(int j = 0; j < size; j++) {
-					block [i + 3 * j] = matrix[i][j];
+		if (columnMapping) {
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < size; j++) {
+					block[i + 3 * j] = matrix[i][j];
 				}
 			}
-		}
-		else{
-			for(int i = 0; i < 3; i++) {
-				for(int j = 0; j < size; j++) {
-					block [size * i + j] = matrix[i][j];
+		} else {
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < size; j++) {
+					block[size * i + j] = matrix[i][j];
 				}
 			}
 		}
 	}
-	
-	public static void copyMatrix(byte[][] in, byte[][] out){
+
+	public static void copyMatrix(byte[][] in, byte[][] out) {
 		for (int i = 0; i < in.length; i++)
 			for (int j = 0; j < in[i].length; j++)
 				out[i][j] = in[i][j];
+	}
+
+	public static boolean readFile(String[] filePath) {
+		boolean returnValue = false;
+		File file = new File(filePath[0]);
+		StringBuffer contents = new StringBuffer();
+		BufferedReader reader = null;
+
+		//Unix
+		if(filePath[0].contains("/")){
+			filePath[0] = filePath[0].split("/")[filePath[0].split("/").length - 1];
+		}
+		//Windows
+		else if(filePath[0].contains("\\")){
+			filePath[0] = filePath[0].split("\\")[filePath[0].split("/").length - 1];		
+		}
+		//relative path
+		else{
+			filePath[0] = filePath[0];						
+		}
+		
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			String text = null;
+
+			// repeat until all lines is read
+			while ((text = reader.readLine()) != null) {
+				contents.append(text).append(
+						System.getProperty("line.separator"));
+			}
+			filePath[1] = contents.toString();
+			returnValue = true;
+		} catch (FileNotFoundException e) {
+			returnValue = false;
+		} catch (IOException e) {
+			returnValue = false;
+		} finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+			} catch (IOException e) {
+				returnValue = false;
+			}
+		}
+		return returnValue;
 	}
 }
